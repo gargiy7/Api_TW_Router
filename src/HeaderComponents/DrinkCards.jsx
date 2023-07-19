555d976c0a1605b58e7a9fe3827c099036a3073c
Gargiy <18011548-Gargiy@users.noreply.replit.com>
2023-07-19 14:32:14 +0000

    beer cards to cart

--- src/HeaderComponents/DrinkCards.jsx
+++ src/HeaderComponents/DrinkCards.jsx
@@ -7,7 +7,6 @@ const DrinkCards = ({
     <div className="w-64 h-88 p-4 m-4 bg-orange-100 rounded-xl shadow-lg  shadow-orange-100 cursor-pointer ">
       <h3 >{strDrink}</h3>
       <img src={strDrinkThumb} />
-
     </div>
   );
 };
